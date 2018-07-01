544fa234ef9459929af54b6b1c6effe22e87dff8
champentrustz <champ.pittaya1995@gmail.com>
2018-07-01 20:29:57 +0700

    add detail in page

--- rule/manage-deduction-criteria.php
+++ rule/manage-deduction-criteria.php
@@ -1,7 +1,217 @@
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Entrustz
- * Date: 7/1/2018
- * Time: 7:27 PM
- */
+session_start();
+date_default_timezone_set("Asia/Bangkok");
+$adminID = $_SESSION['admin_id'];
+if($adminID == null || $adminID == ''){
+    ?>
+    <script>window.location.href='login';</script>
+    <?php
+}
+function ThDate()
+{
+//วันภาษาไทย
+    $ThDay = array ( "อาทิตย์", "จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์", "เสาร์" );
+//เดือนภาษาไทย
+    $ThMonth = array ( "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน","พฤษภาคม", "มิถุนายน", "กรกฏาคม", "สิงหาคม","กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม" );
+
+//กำหนดคุณสมบัติ
+    $week = date( "w" ); // ค่าวันในสัปดาห์ (0-6)
+    $months = date( "m" )-1; // ค่าเดือน (1-12)
+    $day = date( "d" ); // ค่าวันที่(1-31)
+    $years = date( "Y" )+543; // ค่า ค.ศ.บวก 543 ทำให้เป็น ค.ศ.
+
+    return "วัน$ThDay[$week] 
+		ที่ $day  
+		เดือน $ThMonth[$months] 
+		พ.ศ. $years";
+}
+
+$semester = $_REQUEST['semester'];
+$year = $_REQUEST['year'];
+
+if(($semester & $year) == null || ($semester & $year) == ''){
+    $month = date('n');
+    $year = date('Y');
+    if($month>='5'  && $month <='10'){
+        $semester =1;
+
+    }else{
+        $semester =2;
+        if($month >= 1 && $month <5){
+            $year = $year-1;
+        }
+    }
+}
+
+$_SESSION['semester'] = $semester;
+$_SESSION['year'] = $year;
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>C.S.W. Online Behavior</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,300i" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="../css/core.min.css" rel="stylesheet">
+    <link href="../css/app.min.css" rel="stylesheet">
+    <link href="../css/style.min.css" rel="stylesheet">
+
+    <!-- Favicons -->
+</head>
+
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css?family=Athiti');
+    *{
+        font-family: 'Athiti', sans-serif;
+    }
+    .show-font{
+        font-family: 'Athiti', sans-serif;
+    }
+
+    .zoom {
+        -webkit-transition: all 0.35s ease-in-out;
+        -moz-transition: all 0.35s ease-in-out;
+        transition: all 0.35s ease-in-out;
+        cursor: -webkit-zoom-in;
+        cursor: -moz-zoom-in;
+        cursor: zoom-in;
+    }
+
+    .zoom:hover,
+    .zoom:active,
+    .zoom:focus {
+        -ms-transform: scale(2.5);
+        -moz-transform: scale(2.5);
+        -webkit-transform: scale(2.5);
+        -o-transform: scale(2.5);
+        transform: scale(2.5);
+        position:relative;
+        z-index:100;
+    }
+
+</style>
+
+<body>
+
+
+
+<!-- Preloader -->
+<div class="preloader">
+    <div class="spinner-dots">
+        <span class="dot1"></span>
+        <span class="dot2"></span>
+        <span class="dot3"></span>
+    </div>
+</div>
+
+<?php include "side-bar.php";?>
+
+
+<!-- Main container -->
+<main class="main-container">
+
+    <div class="card text-center">
+        <div class="col-md-12">
+            <h2 class="header-title">
+                <p></p>
+                <span class="text-info">จัดการเกณฑ์การตัดคะแนนความประพฤติ</span>
+                <p></p>
+                <!--                <small class="show-font">โรงเรียนชำนาญสามัคคีวิทยา</small>-->
+            </h2 class="header-title ">
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="card">
+            <!--            <h4 class="card-title show-font">รายงานการตัดคะแนนความประพฤติ</h4>-->
+            <div class="card-body">
+
+
+                <button class="btn btn-primary" data-provide="loader" data-url="tb_deduction_criteria.php" data-target="#loader-target" style="display: none"  id="button-late">Button</button>
+                <div id="loader-target">
+
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+</main>
+<!-- END Main container -->
+
+
+
+
+
+<!-- Scripts -->
+<script src="../js/core.min.js"></script>
+<script src="../js/app.min.js"></script>
+<script src="../js/script.min.js"></script>
+
+
+
+<script>
+
+    var click = 0;
+    var toggle = 0;
+
+    setTimeout(function(){  $('#button-late').trigger('click'); }, 200);
+
+    $( "#toggle-fold" ).click(function() {
+        click ++;
+        if(click %2 == 0){
+            $('#profile').show();
+        }
+        else{
+
+            $( document ).on( "mousemove", function( event ) {
+                if(click %2 == 0){
+                    event.stopPropagation();
+                }
+                else{
+                    if(event.pageX <= 80 && event.pageY >= 0){
+                        $('#profile').show('slow');
+
+                    }
+                    if(event.pageX >= 260 && event.pageY >= 0){
+                        $('#profile').hide('slow');
+
+                    }
+                }
+
+            });
+        }
+
+    });
+
+
+</script>
+
+
+</body>
+</html>
--- rule/tb_deduction_criteria.php
+++ rule/tb_deduction_criteria.php
@@ -1,7 +1,137 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Entrustz
- * Date: 7/1/2018
- * Time: 7:31 PM
- */
+session_start();
+include '../config_db.php';
+
+$month = date('n');
+$year = date('Y');
+if($month>='5'  && $month <='10'){
+    $semester =1;
+
+}else{
+    $semester =2;
+    if($month >= 1 && $month <5){
+        $year = $year-1;
+    }
+}
+
+$date_day = $_SESSION['date_day'];
+
+function display_timetype($status){
+    if($status=='1'){
+        $show_status = "<span class='glyphicon glyphicon-ok text-success'></span>";
+    }elseif($status=='2'){
+        $show_status = "สาย";
+    }elseif($status=='3'){
+        $show_status = "ลา";
+    }elseif($status=='4'){
+        $show_status = "ลาป่วย";
+    }elseif($status=='5'){
+        $show_status = "ขาดเรียน";
+    }elseif($status=='6'){
+        $show_status = "ลากิจ";
+    }elseif($status=='7'){
+        $show_status = "กิจกรรม";
+    }
+    return $show_status;
+}
+
+?>
+
+<script>
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );
+</script>
+
+<br/>
+<div class="table-responsive-lg" style="font-size: 16px">
+    <table class="table table-xl table-hover table-bordered fixed" data-page-length='100' data-provide="datatables"  >
+        <thead>
+        <tr>
+            <th  class="text-center" style="font-weight: bold;">ที่</th>
+            <th  class="text-center" style="font-weight: bold;">เกณฑ์</th>
+            <th  class="text-center " style="font-weight: bold;">คะแนน</th>
+            <th  class="text-center"><span class="fa fa-cog"></span></th>
+            <th  class="text-center"><span class="fa fa-cog"></span></th>
+
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+        $sql_rule_type = "SELECT * FROM tb_ruletypes WHERE tb_ruletype_status = 1 ORDER BY tb_ruletype_score asc, tb_ruletype_id asc";
+        $result_rule_type = mysqli_query($conn, $sql_rule_type);
+        $i = 0;
+
+        $count = mysqli_num_rows($result_rule_type);
+        if($count > 0) {
+            while ($row_rule_type = mysqli_fetch_assoc($result_rule_type)) {
+                $i++;
+
+
+                ?>
+                <tr style="font-size: 16px">
+                    <td class="text-center"><?php print $i?></td>
+                    <td width="65%"><?php print $row_rule_type['tb_ruletype_name']?></td>
+                    <td class="text-center"><?php print $row_rule_type['tb_ruletype_score']?> คะแนน</td>
+                    <td class="text-center">
+                        <button class="btn btn-warning btn-sm show-font" data-toggle="modal" data-target="#modal-edit-<?php print $i?>">แก้ไข</button>
+                    </td>
+                    <td>
+                        <button class="btn btn-danger btn-sm show-font" data-provide="tooltip" title="แก้ไข" type="submit">ลบ</button>
+                    </td>
+
+                </tr>
+                <div class="modal fade" id="modal-edit-<?php print $i?>" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title show-font" id="myModalLabel">แก้ไขเกณฑ์การตัดคะแนนความประพฤติ</h4>
+                                <button type="button" class="close" data-dismiss="modal">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body ">
+
+
+                                <form class="form-type-material">
+
+                                    <div class="form-group">
+                                        <label for="rule-name" style="font-size: 14px" class="text-info">เกณฑ์</label>
+                                        <input type="text" class="form-control show-font" value="<?php print $row_rule_type['tb_ruletype_name']?>" id="rule-name">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="rule-score" style="font-size: 14px" class="text-info">คะแนน</label>
+                                        <input type="number" class="form-control col-md-2 show-font" value="<?php print $row_rule_type['tb_ruletype_score']?>" id="rule-score">
+                                    </div>
+
+
+                                </form>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-bold btn-pure btn-secondary show-font" data-dismiss="modal" style="font-size: 16px">ปิด</button>
+                                <button type="button" class="btn btn-bold btn-pure btn-primary show-font" style="font-size: 16px">แก้ไข</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+        }
+
+        ?>
+
+
+        </tbody>
+
+    </table>
+
+
+
+</div>
+<br/>
+
+
