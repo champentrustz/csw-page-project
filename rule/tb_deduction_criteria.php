97dc6dfb8575644083d9ff2f2d9e0b1211636c8b
champentrustz <champ.pittaya1995@gmail.com>
2018-07-01 21:48:13 +0700

    add detail in page

--- rule/tb_deduction_criteria.php
+++ rule/tb_deduction_criteria.php
@@ -37,14 +37,49 @@ function display_timetype($status){
 
 ?>
 
-<script>
-    $(document).ready( function () {
-        $('#myTable').DataTable();
-    } );
-</script>
 
 <br/>
+<button class="btn btn-primary show-font" data-toggle="modal" data-target="#modal-add"><span class="icon ti-plus"></span> เพิ่มเกณฑ์</button>
+
+<div class="modal fade" id="modal-add" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title show-font" id="myModalLabel">เพิ่มเกณฑ์การตัดคะแนนความประพฤติ</h4>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body ">
+
+
+
+                <div class="form-group">
+                    <label for="rule-name-add" style="font-size: 14px" class="text-info">เกณฑ์</label>
+                    <input type="text" name="ruletype_name" class="form-control show-font" id="rule-name-add">
+                </div>
+
+
+
+                <div class="form-group">
+                    <label for="rule-score-add" style="font-size: 14px" class="text-info">คะแนน</label>
+                    <input type="number" name="ruletype_score" class="form-control col-md-2 show-font" id="rule-score-add" value="0">
+                </div>
+
+
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-bold btn-pure btn-secondary show-font" data-dismiss="modal" style="font-size: 16px">ปิด</button>
+                <button type="button" class="btn btn-bold btn-pure btn-primary show-font" style="font-size: 16px" onclick="addRuleType()">บันทึก</button>
+            </div>
+
+        </div>
+    </div>
+</div>
 <div class="table-responsive-lg" style="font-size: 16px">
+    <br/>
     <table class="table table-xl table-hover table-bordered fixed" data-page-length='100' data-provide="datatables"  >
         <thead>
         <tr>
@@ -75,14 +110,14 @@ function display_timetype($status){
                     <td width="65%"><?php print $row_rule_type['tb_ruletype_name']?></td>
                     <td class="text-center"><?php print $row_rule_type['tb_ruletype_score']?> คะแนน</td>
                     <td class="text-center">
-                        <button class="btn btn-warning btn-sm show-font" data-toggle="modal" data-target="#modal-edit-<?php print $i?>">แก้ไข</button>
+                        <button class="btn btn-warning btn-sm show-font" data-toggle="modal" data-target="#modal-edit-<?php print $row_rule_type['tb_ruletype_id']?>">แก้ไข</button>
                     </td>
                     <td>
-                        <button class="btn btn-danger btn-sm show-font" data-provide="tooltip" title="แก้ไข" type="submit">ลบ</button>
+                        <button class="btn btn-danger btn-sm show-font" data-provide="tooltip" title="แก้ไข" type="submit" onclick="deleteRuleType('<?php print $row_rule_type['tb_ruletype_id']?>')">ลบ</button>
                     </td>
 
                 </tr>
-                <div class="modal fade" id="modal-edit-<?php print $i?>" tabindex="-1">
+                <div class="modal fade" id="modal-edit-<?php print $row_rule_type['tb_ruletype_id']?>" tabindex="-1">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
@@ -94,25 +129,26 @@ function display_timetype($status){
                             <div class="modal-body ">
 
 
-                                <form class="form-type-material">
 
                                     <div class="form-group">
-                                        <label for="rule-name" style="font-size: 14px" class="text-info">เกณฑ์</label>
-                                        <input type="text" class="form-control show-font" value="<?php print $row_rule_type['tb_ruletype_name']?>" id="rule-name">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="rule-score" style="font-size: 14px" class="text-info">คะแนน</label>
-                                        <input type="number" class="form-control col-md-2 show-font" value="<?php print $row_rule_type['tb_ruletype_score']?>" id="rule-score">
+                                        <label for="rule-name-<?php print $row_rule_type['tb_ruletype_id']?>" style="font-size: 14px" class="text-info">เกณฑ์</label>
+                                        <input type="text" name="ruletype_name" class="form-control show-font" value="<?php print $row_rule_type['tb_ruletype_name']?>" id="rule-name-<?php print $row_rule_type['tb_ruletype_id']?>">
                                     </div>
 
 
-                                </form>
+
+                                    <div class="form-group">
+                                        <label for="rule-score-<?php print $row_rule_type['tb_ruletype_id']?>" style="font-size: 14px" class="text-info">คะแนน</label>
+                                        <input type="number" name="ruletype_score" class="form-control col-md-2 show-font" value="<?php print $row_rule_type['tb_ruletype_score']?>" id="rule-score-<?php print $row_rule_type['tb_ruletype_id']?>">
+                                    </div>
+
+
+
                             </div>
 
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-bold btn-pure btn-secondary show-font" data-dismiss="modal" style="font-size: 16px">ปิด</button>
-                                <button type="button" class="btn btn-bold btn-pure btn-primary show-font" style="font-size: 16px">แก้ไข</button>
+                                <button type="button" class="btn btn-bold btn-pure btn-primary show-font" style="font-size: 16px" onclick="updateRuleType('<?php print $row_rule_type['tb_ruletype_id']?>')">แก้ไข</button>
                             </div>
 
                         </div>
@@ -134,4 +170,74 @@ function display_timetype($status){
 </div>
 <br/>
 
+<script>
+    async function addRuleType() {
 
+        let name =  $( "#rule-name-add" ).val();
+        let score =  $( "#rule-score-add" ).val();
+
+        const resp = await fetch('https://www.csw.ac.th/csw_api/add_deduction_ruletype.php', {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                ruletype_name :  name,
+                ruletype_score :  score,
+            }),
+        });
+
+        $('#modal-add').modal('hide');
+        setTimeout(function(){ $('#button-late').trigger('click'); }, 300);
+
+    }
+</script>
+
+<script>
+    async function deleteRuleType(id) {
+
+        const resp = await fetch('https://www.csw.ac.th/csw_api/delete_deduction_ruletype.php', {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                ruletype_id :  id,
+            }),
+        });
+
+        $('#button-late').trigger('click');
+
+    }
+</script>
+
+<script>
+    async function updateRuleType(id) {
+
+        let name =  $( "#rule-name-"+id ).val();
+        let score =  $( "#rule-score-"+id ).val();
+
+        const resp = await fetch('https://www.csw.ac.th/csw_api/update_deduction_ruletype.php', {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                ruletype_id :  id,
+                ruletype_name :  name,
+                ruletype_score :  score,
+            }),
+        });
+
+        refreshTabel(resp,id);
+
+    }
+
+
+    function refreshTabel(resp,id)
+    {
+        $('#modal-edit-'+id).modal('hide');
+        setTimeout(function(){ $('#button-late').trigger('click'); }, 300);
+
+    }
+</script>
